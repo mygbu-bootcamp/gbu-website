d7cdd9058ce83472f1d87fd13ef9d11ceb6da375
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-06-17 02:24:51 +0530

    navbar added + board of study component created

--- src/pages/departments/ICTPage.jsx
+++ src/pages/departments/ICTPage.jsx
@@ -1,27 +1,22 @@
-
-
-Landing
-import AboutSection from '../../components/departments/AboutIct';
-import DeanMessage from '../../components/departments/Dean';
-import ProgramsShowcase from '../../components/departments/Program';
-import DepartmentsSection from '../../components/departments/Deptcard';
-import NoticeEvents from '../../components/departments/Notice.jsx';
-import PlacementsSection from '../../components/departments/Placement.jsx';
-import RecentPlacements from '../../components/departments/Recent_Placement.jsx';
-import RecruitersShowcase from '../../components/departments/Recuritor_showcase';
-import ClubsAchievements from '../../components/departments/Clubs_activevment.jsx';
-import StudentAchievements from '../../components/departments/Student_achievements.jsx';
-import IncubationCenter from '../../components/departments/Incubations.jsx';
-import Landing from '../../components/departments/Landing.jsx';
-
-
-
-
+Landing;
+import Navbar from "../../components/departments/Navbar";
+import AboutSection from "../../components/departments/AboutIct";
+import DeanMessage from "../../components/departments/Dean";
+import ProgramsShowcase from "../../components/departments/Program";
+import DepartmentsSection from "../../components/departments/Deptcard";
+import NoticeEvents from "../../components/departments/Notice.jsx";
+import PlacementsSection from "../../components/departments/Placement.jsx";
+import RecentPlacements from "../../components/departments/Recent_Placement.jsx";
+import RecruitersShowcase from "../../components/departments/Recuritor_showcase";
+import ClubsAchievements from "../../components/departments/Clubs_activevment.jsx";
+import StudentAchievements from "../../components/departments/Student_achievements.jsx";
+import IncubationCenter from "../../components/departments/Incubations.jsx";
+import Landing from "../../components/departments/Landing.jsx";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-white">
-
+      <Navbar />
       <Landing />
       <AboutSection />
       <DeanMessage />
@@ -34,7 +29,6 @@ const Index = () => {
       <ClubsAchievements />
       <StudentAchievements />
       <IncubationCenter />
-
     </div>
   );
 };
