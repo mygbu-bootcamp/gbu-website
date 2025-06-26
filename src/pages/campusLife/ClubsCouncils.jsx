33224342c13ae5e38e4a8eb92dc4046b3b30ed79
Neelakshi <neelakshikadyan@gmail.com>
2025-06-26 13:07:00 +0530

    Changed bg image in virtual tour(Landing page) and some pages in announcement

--- src/pages/campusLife/ClubsCouncils.jsx
+++ src/pages/campusLife/ClubsCouncils.jsx
@@ -120,24 +120,6 @@ const ClubsCouncils = () => {
   const [selectedClubToJoin, setSelectedClubToJoin] = useState(null);
   const { toast } = useToast();
 
-<<<<<<< main
-=======
-  const fetchClubs = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_HOST}campuslife/student-clubs/`
-      );
-      setClubs(response.data);
-    } catch (error) {
-      console.error('Error fetching clubs:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchClubs();
-  }, []);
-
->>>>>>> main
   const handleClubClick = (club) => {
     setSelectedClub(club);
   };
