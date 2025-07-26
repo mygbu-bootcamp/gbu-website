4d2a4869f5d7909d0861ac02466f326c300e9bb9
Ashwani Kushwaha <ashwanik346981@gmail.com>
2025-07-26 12:23:45 +0530

    Update Schools

--- src/components/departments/Dean.jsx
+++ src/components/departments/Dean.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LeadershipCard = ({ name, title, image, description }) => {
   const safeName = name || "Dr. Arpit Bhardwaj";
   const safeTitle = title || "Dean, School of Information and Communication Technology, Gautam Buddha University";
   const safeImage = image || "https://www.gbu.ac.in/USICT/media/img/arpit%20bhardwaj.jpg"; 
-  const safeDescription = description || "Dr. Arpit Bhardwaj, Ph.D. (IIT Indore), serves as the Dean of the School of ICT at Gautam Buddha University. With over 50+ research publications in reputed journals like IEEE, Elsevier, and Springer, his research interests include Genetic Programming, Brain-Computer Interface (BCI), and EEG-based emotion recognition.Since the inception of the School in 2008, he has played a vital role in shaping the academic and research environment, overseeing undergraduate, postgraduate, and doctoral programs. Under his leadership, the school emphasizes innovation, interdisciplinary research, and industry-oriented learning with modern labs and regular expert talks.He continues to contribute actively to academia and industry collaborations, guiding students and faculty alike towards excellence in the field of Information and Communication Technology.";
+  const safeDescription = description || `University School of Information and Communication Technology came into existence in 2008 with the primary objective of catering to all academic and research requirements in fields of electronics communication and computer science. The first academic session consisting of post-graduate program started in July 2009, and at present, it is successfully running undergraduate, post-graduate, and research programs in all emerging areas related to information and communication technology. The school is committed towards providing highest quality teaching education and producing technocrats who can work on the latest available technologies in the global context. To fulfill this aim and to provide education according to industrial requirement the extra academic activities like lectures from outside expert / workshops / symposiums / conferences are organized on a regular basis. First batch of postgraduate students has success fully passed in 2011, School has wonderful infrastructure having classrooms equipped with state of the art audio visual support and large number of functional laboratories equipped with state of the are instruments / hardware software and highly committed and dedicated faculty. School has already made a roadmap for its development for next 10 years in the form of “Vision 2020” document and is in real spirit marching on this roadmap for fulfilling the defined objectives. School aims to become a hub of higher end research activities and has started successfully its research program and have many research scholars on its roll, carrying research in all prominent areas of electronics / communication / computer engineering.\n\nI am more than confident to tell you that the best, most effective and finest weapon an academic organization can ever have is “knowledge” and it is a common quote in ICT field that “knowledge is power”. The USICT works on the sole objective of acquiring and propagating as much knowledge as possible to the students.`;
 
   return (
     <section className="py-12 sm:py-16 ">
@@ -23,13 +24,13 @@ const LeadershipCard = ({ name, title, image, description }) => {
             className="w-60 h-56 sm:w-48 sm:h-64 md:w-[220px] md:h-[300px] object-cover rounded-xl shadow-md"
           />
           <div className="text-center md:text-left">
-            <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-blue-900">
+            <Link to="/academics/faculty/2"><h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-blue-900">
               {safeName}
-            </h3>
+            </h3></Link>
             <p className="text-sm sm:text-base text-gray-600 mb-2 sm:mb-3">
               {safeTitle}
             </p>
-            <p className="text-gray-700 text-sm sm:text-base whitespace-pre-line">
+            <p className="text-gray-700 text-sm sm:text-base whitespace-pre-line text-justify">
               {safeDescription}
             </p>
           </div>
